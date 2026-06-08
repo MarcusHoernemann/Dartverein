84d512ebca49902ed22362532eae7a8137e95122
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-06-08 19:09:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
 self.addEventListener('install', (event) => {
-    // Sofort aktivieren, ohne auf alte Caches zu warten
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    // Kontrolle über alle Clients übernehmen
     event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-    // Einfach alles normal laden, damit die PWA-Pflicht erfüllt ist
     event.respondWith(fetch(event.request));
 });
